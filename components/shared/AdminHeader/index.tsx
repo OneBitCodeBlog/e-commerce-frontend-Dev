61081e083bae6f5d9793eeb0a1d723b5cb2ef952
Lucas Siqueira <Lucassiqueirafernandes07@gmail.com>
2020-11-19 09:46:27 -0300

    Setting Admin Header Links

--- components/shared/AdminHeader/index.tsx
+++ components/shared/AdminHeader/index.tsx
@@ -3,6 +3,7 @@ import { Row, Col } from 'react-bootstrap';
 import styles from '../../../styles/AdminHeader.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignal, faUser, faGamepad, faCheckSquare, faLaptop, faTicketAlt, faDollarSign, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
 interface AdminHeaderProps {
     name: string
@@ -12,13 +13,13 @@ const AdminHeader: React.FC<AdminHeaderProps> = ({name}) => {
     return(
         <Row className={styles.background}>
             <Col lg={6} xs={9}>
-                <FontAwesomeIcon icon={faSignal} color="var(--color-gray-light)" className="ml-3" />
-                <FontAwesomeIcon icon={faUser} color="var(--color-gray-light)" className="ml-3" />
-                <FontAwesomeIcon icon={faGamepad} color="var(--color-gray-light)" className="ml-3" />
-                <FontAwesomeIcon icon={faCheckSquare} color="var(--color-gray-light)" className="ml-3" />
-                <FontAwesomeIcon icon={faLaptop} color="var(--color-gray-light)" className="ml-3" />
-                <FontAwesomeIcon icon={faTicketAlt} color="var(--color-gray-light)" className="ml-3" />
-                <FontAwesomeIcon icon={faDollarSign} color="var(--color-gray-light)" className="ml-3" />
+                <Link href="/"><a><FontAwesomeIcon icon={faSignal} color="var(--color-gray-light)" className="ml-3" /></a></Link>
+                <Link href="/Admin/Users/List"><a><FontAwesomeIcon icon={faUser} color="var(--color-gray-light)" className="ml-3" /></a></Link>
+                <Link href="/Admin/Products/List"><a><FontAwesomeIcon icon={faGamepad} color="var(--color-gray-light)" className="ml-3" /></a></Link>
+                <Link href="/Admin/Category/List"><a><FontAwesomeIcon icon={faCheckSquare} color="var(--color-gray-light)" className="ml-3" /></a></Link>
+                <Link href="/Admin/SystemRequirements/List"><a><FontAwesomeIcon icon={faLaptop} color="var(--color-gray-light)" className="ml-3" /></a></Link>
+                <Link href="/Admin/Coupon/List"><a><FontAwesomeIcon icon={faTicketAlt} color="var(--color-gray-light)" className="ml-3" /></a></Link>
+                <Link href="#"><a><FontAwesomeIcon icon={faDollarSign} color="var(--color-gray-light)" className="ml-3" /></a></Link>
             </Col>
 
             <Col lg={6} xs={3} className="d-none d-md-block">
