a02bcf94f58b8089aef47fd1ccee574aa4de1fdc
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2020-11-20 20:00:06 -0300

    Adding sign-up feature

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import MainComponent from '../components/shared/MainComponent';
+
+import { useRouter } from 'next/router';
+
+const Home: React.FC = () => {
+  //TODO home page
+
+  const router = useRouter();
+
+  return (
+    <MainComponent>
+      <h1>Home Page</h1>
+
+      <button onClick={() => router.push('/Auth/Login')}>Login</button>
+      <button onClick={() => router.push('/Auth/Register')}>Register</button>
+    </MainComponent>
+  )
+}
+
+export default Home;
