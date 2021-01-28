80b3c3392384f065c3431dbd093e309f34f7e97e
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-01-28 00:24:01 -0300

    Adding footer links, adjusting responsivity and adding anchor active effects

--- components/shared/Footer/StorefrontFooter/index.tsx
+++ components/shared/Footer/StorefrontFooter/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import styles from './styles.module.css';
 import { Col, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faFacebookF, faInstagram, faYoutube, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faInstagram, faYoutube, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import Logo from '../../Logo';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const StorefrontFooter: React.FC = () => {
     return (
@@ -18,47 +19,83 @@ const StorefrontFooter: React.FC = () => {
                             </Col>
 
                             <Col lg={7} md={12} className="mb-4 mb-lg-0">
-                                <Row>
+                                <Row className={styles.social_medias}>
                                     <Col lg={1} xs={2}>
-                                        <FontAwesomeIcon icon={faFacebookF}  />
+                                        <Link href="https://www.facebook.com/onebitcode/">
+                                            <a target="_blank">
+                                                <FontAwesomeIcon icon={faFacebookF} />
+                                            </a>
+                                        </Link>
                                     </Col>
 
                                     <Col lg={1} xs={2}>
-                                        <FontAwesomeIcon icon={faInstagram} />
+                                        <Link href="https://www.instagram.com/onebitcode/">
+                                            <a target="_blank">
+                                                <FontAwesomeIcon icon={faInstagram} />
+                                            </a>
+                                        </Link>
                                     </Col>
 
                                     <Col lg={1} xs={2}>
-                                        <FontAwesomeIcon icon={faYoutube}  />
+                                        <Link href="https://www.youtube.com/onebitcode">
+                                            <a target="_blank">
+                                                <FontAwesomeIcon icon={faYoutube} />
+                                            </a>
+                                        </Link>
                                     </Col>
 
                                     <Col lg={1} xs={2}>
-                                        <FontAwesomeIcon icon={faTwitter}  />
+                                        <Link href="https://twitter.com/onebitcode">
+                                            <a target="_blank">
+                                                <FontAwesomeIcon icon={faTwitter} />
+                                            </a>
+                                        </Link>
                                     </Col>
 
                                     <Col lg={1} xs={2}>
-                                        <FontAwesomeIcon icon={faLinkedin}  />
+                                        <Link href="https://www.linkedin.com/in/leonardo-scorza-onebitcode-7aba352b/">
+                                            <a target="_blank">
+                                                <FontAwesomeIcon icon={faLinkedin} />
+                                            </a>
+                                        </Link>
                                     </Col>
                                 </Row>
                             </Col>
                         </Row>
                     </Col>
 
-                    <Col lg={{span: 4, offset: 0}} xs={{span: 8, offset: 2}}>
+                    <Col lg={{ span: 4, offset: 0 }} xs={{ span: 8, offset: 2 }}>
                         <Row>
                             <Col>
-                                Contato
+                                <Link href="https://www.onebitcode.com">
+                                    <a target="_blank">
+                                        Contato
+                                    </a>
+                                </Link>
                             </Col>
 
                             <Col>
-                                Sobre
+                                <Link href="https://www.onebitcode.com">
+                                    <a target="_blank">
+                                        Sobre
+                                    </a>
+                                </Link>
                             </Col>
 
                             <Col>
-                                Blog
+                                <Link href="https://www.onebitcode.com">
+                                    <a target="_blank">
+                                        Blog
+                                    </a>
+                                </Link>
                             </Col>
 
                             <Col>
-                                FAQ
+                                <Link href="https://www.onebitcode.com">
+                                    <a target="_blank">
+                                        FAQ
+                                    </a>
+                                </Link>
                             </Col>
                         </Row>
                     </Col>
@@ -67,15 +104,15 @@ const StorefrontFooter: React.FC = () => {
                 <hr className={styles.line} />
 
                 <Row>
-                    <Col lg={{span: 2, offset: 0}} xs={{span: 8, offset: 2}}>
+                    <Col className={styles.logo} lg={{ span: 2, offset: 0 }} xs={{ span: 8, offset: 2 }}>
                         <Logo />
                     </Col>
 
-                    <Col lg={{span: 6, offset: 1}} xs={12}>
-                        <p style={{'color': 'var(--color-gray-light)'}}>onebitcode.com • contato@onebitcode.com</p>
+                    <Col className="text-center" lg={{ span: 6, offset: 1 }} xs={12}>
+                        <p style={{ 'color': 'var(--color-gray-light)' }}>onebitcode.com • contato@onebitcode.com</p>
                     </Col>
 
-                    <Col lg={{span: 2, offset: 1}} xs={{span: 6, offset: 3}}>
+                    <Col lg={{ span: 2, offset: 1 }} xs={{ span: 6, offset: 3 }}>
                         <Image src="/assets/logo-bootcamp.png" alt="Logo Bootcamp" width={240} height={70} />
                     </Col>
                 </Row>
