fb0573990bf0125d382768ef9934d95566c5182d
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-01-28 11:42:28 -0300

    Adjusting Storefront home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,45 @@
 import MainComponent from '../components/shared/MainComponent';
 import HighlightedProducts from '../components/Storefront/HighlightedProducts';
+import { Carousel } from 'react-bootstrap';
+import styles from './styles.module.css';
+import HightlightedProducts from '../components/Storefront/HighlightedProducts';
 
-const Home: React.FC = () => {
+const Storefront: React.FC = () => {
   return (
     <MainComponent>
-      <h1>Home</h1>
-      <HighlightedProducts title="Em destaque"/>
+      <Carousel className={styles.carousel}>
+        <Carousel.Item>
+          <img 
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg" 
+            alt="First Slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img 
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg" 
+            alt="Second Slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img 
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg" 
+            alt="Third Slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
+      <HightlightedProducts title="Ofertas da semana" type="highlighted"/>
+
+      <HightlightedProducts title="Lançamentos"/>
+
+      <HightlightedProducts title="Mais populares"/>
     </MainComponent>
   )
 }
 
-export default Home;
+export default Storefront;
