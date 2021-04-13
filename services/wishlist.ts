d4aebf131792ac82cb1307e4516d3e2a4e3c6e95
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-04-13 00:26:16 -0300

    Adding index method WishlistService

--- services/wishlist.ts
+++ services/wishlist.ts
@@ -1,6 +1,15 @@
 import api from './api';
+import ProductSearch from '../dtos/ProductSearch';
+
+interface WishlistIndexData {
+  wish_items: ProductSearch[];
+}
 
 const WishlistService = {
+  index(url: string) {
+    return api.get<WishlistIndexData>(url).then(response => response.data);
+  },
+
   add(productId: number) {
     return api.post<void>('/storefront/v1/wish_items', { wish_item: { product_id: productId } });
   }
