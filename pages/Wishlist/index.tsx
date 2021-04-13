7a5fd844153a30db9fb4724cc325d8f4d6328326
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-04-13 00:34:51 -0300

    Adding Wishlist page items exhibition

--- pages/Wishlist/index.tsx
+++ pages/Wishlist/index.tsx
@@ -12,78 +12,73 @@ import Menu from '../../components/Storefront/Menu';
 
 import styles from './styles.module.css';
 
+import useSwr from 'swr';
+import WishlistService from '../../services/wishlist';
+
+import { toast } from 'react-toastify';
+
 const Wishlist: React.FC = () => {
+  const { data, error } = useSwr('/storefront/v1/wish_items', WishlistService.index);
+
+  if (error) {
+    toast.error('Erro ao obter os games desejados.');
+    console.log(error);
+  }
+
   return (
     <MainComponent>
       <Menu tab="desired_games"/>
 
-      <div className="mt-4 mb-4">
-        <BlueBackground>
-          <Row>
-            <Col md={2} xs={4} className="text-center pt-4">
-              <FontAwesomeIcon icon={faHeart} size="lg" />
-            </Col>
+    {
+      data?.wish_items?.map(
+        wish_item =>
+          <div className="mt-4 mb-4" key={wish_item.id}>
+            <BlueBackground>
+              <Row>
+                <Col md={2} xs={4} className="text-center pt-4">
+                  <FontAwesomeIcon icon={faHeart} size="lg" />
+                </Col>
 
-            <Col md={3} xs={8}>
-              <img 
-                className={styles.product_image}
-                src="/assets/product_image.png" 
-                alt="Counter Strike"
-              />
-            </Col>
+                <Col md={3} xs={8}>
+                  <img 
+                    className={styles.product_image}
+                    src={wish_item.image_url} 
+                    alt={wish_item.name}
+                  />
+                </Col>
 
-            <Col md={4} xs={12} className={styles.product_data}>
-              <h6 className="font-weight-bold">Counter Strike</h6>
+                <Col md={4} xs={12} className={styles.product_data}>
+                  <h6 className="font-weight-bold">{wish_item.name}</h6>
 
-              <Badge variant="primary ml-1" className={styles.primary_badge}>Ação</Badge>
-              <Badge variant="primary ml-1" className={styles.primary_badge}>Aventura</Badge>
-              <Badge variant="primary ml-1" className={styles.primary_badge}>Indie</Badge>
-            </Col>
+                  {
+                    wish_item?.categories?.map(
+                      category => 
+                        <Badge 
+                          key={category.id}
+                          variant="primary ml-1" 
+                          className={styles.primary_badge}>
+                            {category.name}
+                        </Badge>
+                    )
+                  }
+                </Col>
 
-            <Col md={3} xs={12} className={styles.action}>
-              <strong>R$ 89,90</strong>
+                <Col md={3} xs={12} className={styles.action}>
+                  <strong>{`R$ ${wish_item.price}`}</strong>
 
-              <div>
-                <StyledButton action={"Comprar"} icon={faCartPlus} type_button="blue" />
-              </div>
-            </Col>
-          </Row>
-        </BlueBackground>
-      </div>
-
-      <div className="mt-4 mb-4">
-        <BlueBackground>
-          <Row>
-            <Col md={2} xs={4} className="text-center pt-4">
-              <FontAwesomeIcon icon={faHeart} size="lg" />
-            </Col>
-
-            <Col md={3} xs={8}>
-              <img 
-                className={styles.product_image}
-                src="/assets/product_image.png" 
-                alt="Counter Strike"
-              />
-            </Col>
-
-            <Col md={4} xs={12} className={styles.product_data}>
-              <h6 className="font-weight-bold">Counter Strike</h6>
-
-              <Badge variant="primary ml-1" className={styles.primary_badge}>Ação</Badge>
-              <Badge variant="primary ml-1" className={styles.primary_badge}>Aventura</Badge>
-              <Badge variant="primary ml-1" className={styles.primary_badge}>Indie</Badge>
-            </Col>
-
-            <Col md={3} xs={12} className={styles.action}>
-              <strong>R$ 89,90</strong>
-
-              <div>
-                <StyledButton action={"Comprar"} icon={faCartPlus} type_button="blue" />
-              </div>
-            </Col>
-          </Row>
-        </BlueBackground>
-      </div>
+                  <div>
+                    <StyledButton 
+                      action={"Comprar"} 
+                      icon={faCartPlus} 
+                      type_button="blue" 
+                    />
+                  </div>
+                </Col>
+              </Row>
+            </BlueBackground>
+          </div>
+      )
+    }
     </MainComponent>
   );
 }
