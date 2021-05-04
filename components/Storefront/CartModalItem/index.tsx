cb0f7257f22f5aa87af995e178681f9eb339bbf5
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-05-04 08:25:24 -0300

    Adding CartModalItem props and product info

--- components/Storefront/CartModalItem/index.tsx
+++ components/Storefront/CartModalItem/index.tsx
@@ -3,36 +3,51 @@ import { Row, Col, Badge } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-const CartModalItem: React.FC = () => {
+import ProductShow from '../../../dtos/ProductShow';
+import { MouseEventHandler } from 'react';
+
+interface CartModalItemProps {
+  product: ProductShow;
+  handleRemove: MouseEventHandler<SVGSVGElement>;
+}
+
+const CartModalItem: React.FC<CartModalItemProps> = ({product, handleRemove}) => {
   return (
     <div className={styles.container}>
       <Row>
         <Col xs={5}>
-          <img src="/assets/product_image.png" alt="Product Image"/>
+          <img 
+            src={product.image_url} 
+            alt={product.name}
+          />
         </Col>
 
         <Col xs={7}>
           <p className={styles.name}>
-            God of War
+            {product.name}
           </p>
 
           <div className={styles.badges_container}>
-            <Badge variant="primary ml-1" className={styles.badge}>
-              Ação
-            </Badge>
-
-            <Badge variant="primary ml-1" className={styles.badge}>
-              Ação
-            </Badge>
-
-            <Badge variant="primary ml-1" className={styles.badge}>
-              Ação
-            </Badge>
+            {
+              product?.categories?.filter((_, index) => index < 2)?.map(
+                category => 
+                  <Badge 
+                    key={category.id}
+                    variant="primary ml-1" 
+                    className={styles.badge}
+                  >
+                    {category.name}
+                  </Badge>
+                )
+            }
           </div>
 
           <div className={styles.price_container}>
-            <span>R$ 299,99</span>
-            <FontAwesomeIcon icon={faTrash}/>
+            <span>{`R$ ${product.price}`}</span>
+            <FontAwesomeIcon 
+              icon={faTrash}
+              onClick={handleRemove}
+            />
           </div>
         </Col>
       </Row>
