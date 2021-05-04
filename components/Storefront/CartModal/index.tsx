036c2301ffb41976ecf659ffa2173f09250bae7f
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-05-04 08:16:40 -0300

    Adding CartProducts cartProducts state

--- components/Storefront/CartModal/index.tsx
+++ components/Storefront/CartModal/index.tsx
@@ -3,26 +3,47 @@ import CartModalItem from '../CartModalItem';
 import StyledButton from '../../shared/StyledButton';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCartProduct } from '../../../store/modules/storefront/cartProducts/reducer';
+import ProductShow from '../../../dtos/ProductShow';
+import { useRouter } from 'next/router';
+
 interface CartModalProps {
   searchPage?: boolean;
 }
 
 const CartModal: React.FC<CartModalProps> = ({searchPage = false}) => {
+  const cartProducts: ProductShow[] = useSelector(state => state.cartProducts);
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const handleRemove = (index: number): void => {
+    dispatch(removeCartProduct(index));
+  }
+
   return  (
     <div className={`${styles.container} ${searchPage ? styles.search_page : ''}`}>
       <div className={styles.items}>
-        <CartModalItem />
-        <CartModalItem />
-        <CartModalItem />
-        <CartModalItem />
-        <CartModalItem />
+        {
+          cartProducts?.map(
+            (product, index) =>
+              <CartModalItem 
+                key={index}
+                product={product}
+                handleRemove={() => handleRemove(index)}
+              />
+          )
+        }
+              
       </div>
 
       <div className={styles.separator} />
 
       <div className={styles.actions}>
         <span>
-          R$ 999,99
+          {
+            `R$ ${cartProducts?.reduce((acc, item) => acc + item.price, 0).toFixed(2)}`
+          }
         </span>
 
         <StyledButton 
@@ -30,6 +51,7 @@ const CartModal: React.FC<CartModalProps> = ({searchPage = false}) => {
           action="Finalizar Compra" 
           icon={faShoppingCart} 
           className="btn btn-danger" 
+          onClick={() => router.push('/Cart')}
         />
       </div>
     </div>
