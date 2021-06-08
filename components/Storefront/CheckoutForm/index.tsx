96899222344e5750fd2511089da5dccf15359438
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-06-08 06:17:16 -0300

    Adding CheckoutForm local states

--- components/Storefront/CheckoutForm/index.tsx
+++ components/Storefront/CheckoutForm/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Col, Row, Form } from 'react-bootstrap';
 import BlueBackground from '../../shared/BlueBackground';
 import StyledButton from '../../shared/StyledButton';
@@ -11,6 +12,26 @@ import MaskedInput from 'react-text-mask';
 import MonthsService from '../../../util/MonthsService';
 
 const CheckoutForm: React.FC = () => {
+  const [document, setDocument] = useState('');
+  const [street, setStreet] = useState('');
+  const [number, setNumber] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [postCode, setPostCode] = useState('');
+
+  const [paymentType, setPaymentType] = useState('billet');
+  const [cardNumber, setCardNumber] = useState('');
+  const [expirationMonth, setExpirationMonth] = useState('');
+  const [expirationYear, setExpirationYear] = useState('');
+  const [securityCode, setSecurityCode] = useState('');
+  const [holderName, setHolderName] = useState('');
+  const [installments, setInstallments] = useState('');
+
+  const [creditCard, setCreditCard] = useState(false);
+
+  useEffect(() => {
+    setCreditCard(paymentType === 'credit_card');
+  }, [paymentType])
 
   const handleSubmit = async (evt: React.FormEvent) => {
     evt.preventDefault();
@@ -34,6 +55,11 @@ const CheckoutForm: React.FC = () => {
           type="text" 
           className={`${styles.gray_input} w-100 mb-4`}
           placeholder="CPF/CNPJ"
+          value={document}
+          onChange={
+            (evt: React.ChangeEvent<HTMLInputElement>) => 
+              setDocument(evt.target.value)
+          }
           mask={documentMask}
           required
         />
@@ -48,6 +74,11 @@ const CheckoutForm: React.FC = () => {
               type="radio" 
               className="mr-2" 
               name="payment-type" 
+              checked={paymentType === 'billet'}
+              onChange={
+                (_: React.ChangeEvent<HTMLInputElement>) =>
+                  setPaymentType('billet')
+              }
             />
             <strong>Boleto</strong>
           </div>
@@ -67,6 +98,11 @@ const CheckoutForm: React.FC = () => {
               type="radio" 
               className="mr-2" 
               name="payment-type" 
+              checked={paymentType === 'credit_card'}
+              onChange={
+                (_: React.ChangeEvent<HTMLInputElement>) =>
+                  setPaymentType('credit_card')
+              }
             />
             <strong>Cartão de Crédito</strong>
 
@@ -90,156 +126,238 @@ const CheckoutForm: React.FC = () => {
           />
         </div>
 
-        <div className="mt-4">
-          <strong>Número do cartão</strong>
+        {
+          paymentType === 'credit_card' &&
+          <>
+            <div className="mt-4">
+              <strong>Número do cartão</strong>
 
-          <MaskedInput 
-            type="text" 
-            placeholder="XXXX XXXX XXXX XXXX" 
-            className={styles.gray_input} 
-            mask={[
-              /\d/, /\d/, /\d/, /\d/, ' ',
-              /\d/, /\d/, /\d/, /\d/, ' ',
-              /\d/, /\d/, /\d/, /\d/, ' ',
-              /\d/, /\d/, /\d/, /\d/
-            ]}
-          />
-        </div>
+              <MaskedInput 
+                type="text" 
+                placeholder="XXXX XXXX XXXX XXXX" 
+                className={styles.gray_input} 
+                value={cardNumber}
+                onChange={
+                  (evt: React.ChangeEvent<HTMLInputElement>) => 
+                    setCardNumber(evt.target.value)
+                }
+                required={creditCard}
+                disabled={!creditCard}
+                mask={[
+                  /\d/, /\d/, /\d/, /\d/, ' ',
+                  /\d/, /\d/, /\d/, /\d/, ' ',
+                  /\d/, /\d/, /\d/, /\d/, ' ',
+                  /\d/, /\d/, /\d/, /\d/
+                ]}
+              />
+            </div>
 
-        <div className="mt-3">
-          <Row>
-            <Col xs={8}>
-              <strong>Data de Validade</strong>
-
+            <div className="mt-3">
               <Row>
-                <Col>
-                  <select 
-                    className={styles.gray_select}
-                  >
-                    <option value="">Mês</option>
-                    {
-                      MonthsService.execute().map(
-                        (month, index) => 
-                          <option 
-                            key={index}
-                            value={index + 1}
-                          >
-                            {month}
-                          </option>
-                      )
-                    }
-                  </select>
+                <Col xs={8}>
+                  <strong>Data de Validade</strong>
+
+                  <Row>
+                    <Col>
+                      <select 
+                        className={styles.gray_select}
+                        value={expirationMonth}
+                        onChange={
+                          (evt: React.ChangeEvent<HTMLSelectElement>) => 
+                            setExpirationMonth(evt.target.value)
+                        }
+                        required={creditCard}
+                        disabled={!creditCard}
+                      >
+                        <option value="">Mês</option>
+                        {
+                          MonthsService.execute().map(
+                            (month, index) => 
+                              <option 
+                                key={index}
+                                value={index + 1}
+                              >
+                                {month}
+                              </option>
+                          )
+                        }
+                      </select>
+                    </Col>
+
+                    <Col>
+                      <select 
+                        className={styles.gray_select}
+                        value={expirationYear}
+                        onChange={
+                          (evt: React.ChangeEvent<HTMLSelectElement>) => 
+                            setExpirationYear(evt.target.value)
+                        }
+                        required={creditCard}
+                        disabled={!creditCard}
+                      >
+                        <option value="">Ano</option>
+                        {
+                          new Array(6).fill(0).map(
+                            (_, index) =>
+                              <option
+                                key={index}
+                                value={new Date().getFullYear() + index}
+                              >
+                                {new Date().getFullYear() + index}
+                              </option>
+                          )
+                        }
+                      </select>
+                    </Col>
+                  </Row>
                 </Col>
 
-                <Col>
-                  <select 
-                    className={styles.gray_select}
-                  >
-                    <option value="">Ano</option>
-                    {
-                      new Array(6).fill(0).map(
-                        (_, index) =>
-                          <option
-                            key={index}
-                            value={new Date().getFullYear() + index}
-                          >
-                            {new Date().getFullYear() + index}
-                          </option>
-                      )
+                <Col xs={4}>
+                  <strong>Código</strong>
+                  <MaskedInput 
+                    type="text" 
+                    placeholder="XXX" 
+                    className={`${styles.gray_input} w-100`} 
+                    value={securityCode}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setSecurityCode(evt.target.value)
                     }
-                  </select>
+                    required={creditCard}
+                    disabled={!creditCard}
+                    mask={[/\d/, /\d/, /\d/]}
+                  />
                 </Col>
               </Row>
-            </Col>
 
-            <Col xs={4}>
-              <strong>Código</strong>
-              <MaskedInput 
-                type="text" 
-                placeholder="XXX" 
-                className={`${styles.gray_input} w-100`} 
-                mask={[/\d/, /\d/, /\d/]}
-              />
-            </Col>
-          </Row>
+              <div className="mt-4">
+                <strong>Nome do Cartão</strong>
+                <input 
+                  type="text" 
+                  placeholder="NOME DO CARTÃO" 
+                  className={`${styles.gray_input} w-100`}
+                  value={holderName}
+                  onChange={
+                    (evt: React.ChangeEvent<HTMLInputElement>) => 
+                      setHolderName(evt.target.value)
+                  }
+                  required={creditCard}
+                  disabled={!creditCard} 
+                />
+              </div>
 
-          <div className="mt-4">
-            <strong>Nome do Cartão</strong>
-            <input 
-              type="text" 
-              placeholder="NOME DO CARTÃO" 
-              className={`${styles.gray_input} w-100`} 
-            />
-          </div>
+              <div className="mt-4">
+                <strong>Parcelas</strong>
+                <select 
+                  className={styles.gray_select} 
+                  value={installments}
+                  onChange={
+                    (evt: React.ChangeEvent<HTMLSelectElement>) => 
+                      setInstallments(evt.target.value)
+                  }
+                  required={creditCard}
+                  disabled={!creditCard}
+                >
+                  <option value="">Selecione</option>
+                  <option>1x 188.82 (188.82)</option>
+                </select>
+              </div>
+            </div>
 
-          <div className="mt-4">
-            <strong>Parcelas</strong>
-            <select 
-              className={styles.gray_select} 
-            >
-              <option value="">Selecione</option>
-              <option>1x 188.82 (188.82)</option>
-            </select>
-          </div>
-        </div>
+            <div className="mt-4">
+              <strong>Endereço de cobrança</strong>
 
-        <div className="mt-4">
-          <strong>Endereço de cobrança</strong>
+              <hr className={styles.line} />
 
-          <hr className={styles.line} />
+              <div className="mt-2">
+                <strong>Logradouro</strong>
+                <input 
+                  type="text" 
+                  placeholder="Logradouro" 
+                  className={`${styles.gray_input} w-100`} 
+                  value={street}
+                  onChange={
+                    (evt: React.ChangeEvent<HTMLInputElement>) => 
+                      setStreet(evt.target.value)
+                  }
+                  required={creditCard}
+                  disabled={!creditCard}
+                />
+              </div>
 
-          <div className="mt-2">
-            <strong>Logradouro</strong>
-            <input 
-              type="text" 
-              placeholder="Logradouro" 
-              className={`${styles.gray_input} w-100`} 
-            />
-          </div>
+              <Row className="mt-4">
+                <Col>
+                  <strong>Número</strong>
+                  <input 
+                    type="text" 
+                    placeholder="Número" 
+                    className={`${styles.gray_input} w-100`}
+                    value={number}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setNumber(evt.target.value)
+                    }
+                    required={creditCard}
+                    disabled={!creditCard} 
+                  />
+                </Col>
 
-          <Row className="mt-4">
-            <Col>
-              <strong>Número</strong>
-              <input 
-                type="text" 
-                placeholder="Número" 
-                className={`${styles.gray_input} w-100`} 
-              />
-            </Col>
+                <Col>
+                  <strong>Cidade</strong>
+                  <input 
+                    type="text" 
+                    placeholder="Cidade" 
+                    className={`${styles.gray_input} w-100`} 
+                    value={city}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setCity(evt.target.value)
+                    }
+                    required={creditCard}
+                    disabled={!creditCard}
+                  />
+                </Col>
+              </Row>
 
-            <Col>
-              <strong>Cidade</strong>
-              <input 
-                type="text" 
-                placeholder="Cidade" 
-                className={`${styles.gray_input} w-100`} 
-              />
-            </Col>
-          </Row>
+              <Row className="mt-4">
+                <Col>
+                  <strong>Estado</strong>
+                  <MaskedInput
+                    type="text" 
+                    placeholder="Estado" 
+                    className={`${styles.gray_input} w-100`} 
+                    mask={[/[a-zA-Z]/, /[a-zA-Z]/]}
+                    value={state}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setState(evt.target.value)
+                    }
+                    required={creditCard}
+                    disabled={!creditCard}
+                  />
+                </Col>
 
-          <Row className="mt-4">
-            <Col>
-              <strong>Estado</strong>
-              <MaskedInput
-                type="text" 
-                placeholder="Estado" 
-                className={`${styles.gray_input} w-100`} 
-                mask={[/[a-zA-Z]/, /[a-zA-Z]/]}
-              />
-            </Col>
+                <Col>
+                  <strong>CEP</strong>
+                  <MaskedInput 
+                    type="text" 
+                    placeholder="00000-000" 
+                    className={`${styles.gray_input} w-100`} 
+                    mask={[/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/]}
+                    value={postCode}
+                    onChange={
+                      (evt: React.ChangeEvent<HTMLInputElement>) => 
+                        setPostCode(evt.target.value)
+                    }
+                    required={creditCard}
+                    disabled={!creditCard}
+                  />
+                </Col>
+              </Row>
 
-            <Col>
-              <strong>CEP</strong>
-              <MaskedInput 
-                type="text" 
-                placeholder="00000-000" 
-                className={`${styles.gray_input} w-100`} 
-                mask={[/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/]}
-              />
-            </Col>
-          </Row>
-
-        </div>
+            </div>
+          </>
+        }
       </BlueBackground>
 
       <div className="mt-3" />
