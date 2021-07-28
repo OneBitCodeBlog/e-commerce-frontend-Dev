ccae57d9ab70633a0bb015d787343658a67d58ad
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-07-28 12:27:01 -0300

    Refactoring PaymentConfirmation page

--- pages/PaymentConfirmation/[id].tsx
+++ pages/PaymentConfirmation/[id].tsx
@@ -6,9 +6,7 @@ import styles from './styles.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faGamepad, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 
-import BlueBackground from "../../components/shared/BlueBackground";
-
-import { Col, Row, Badge } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 import withAuth from "../../components/withAuth";
 
@@ -17,6 +15,8 @@ import { useRouter } from 'next/router';
 
 import OrderService from "../../services/order";
 import { toast } from "react-toastify";
+
+import OrderDetail from '../../components/shared/OrderDetail';
 
 const PaymentConfirmation: React.FC = () => {
   const router = useRouter();
@@ -57,56 +57,7 @@ const PaymentConfirmation: React.FC = () => {
         </div>
       </div>
 
-      <BlueBackground>
-        <strong>PRODUTO</strong>
-        <strong className="float-right">VALOR</strong>
-
-        {
-          data?.line_items?.map(
-            (item, index) => (
-              <div key={index}>
-                <hr className={styles.line} />
-
-                <Row className={styles.product}>
-                  <Col sm={3} xs={12} className="text-center">
-                    <img 
-                      src={item?.image_url} 
-                      alt={item?.product}
-                    />
-                  </Col>
-
-                  <Col sm={7} xs={9}>
-                    <strong className={styles.product_name}>
-                      {item?.product}
-                    </strong>
-
-                    <div>
-                      {
-                        item?.categories?.map(
-                          (category, index) =>
-                            <Badge 
-                              key={index}
-                              variant="primary ml-1" 
-                              className={styles.primary_badge}
-                            >
-                              {category}
-                            </Badge>
-                        )
-                      }
-                    </div>
-                  </Col>
-
-                  <Col sm={2} xs={3} className="text-center">
-                    <strong className={styles.price}>
-                      {item?.payed_price}
-                    </strong>
-                  </Col>
-                </Row>
-              </div>
-            )
-          )
-        }
-      </BlueBackground>
+      <OrderDetail items={data?.line_items}/>
 
       <Row className="mt-4 mb-4">
         <Col>
